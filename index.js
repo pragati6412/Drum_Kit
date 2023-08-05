cda5a4a4818bdc0cc4409a67b881c9c8d4acd8e5
Pragati Prakash <88521810+pragati6412@users.noreply.github.com>
2023-08-05 12:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ for (var i = 0; i < n; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         var exp=this.innerHTML;
         if (exp==="w"){
-            var audio1=new Audio('sounds/crash.mp3');
+            var audio1=new Audio('crash.mp3');
             audio1.play();
         }
         else if (exp==="a"){
-            var audio2=new Audio('sounds/kick-bass.mp3');
+            var audio2=new Audio('kick-bass.mp3');
             audio2.play();
         }
         else if (exp==="s"){
-            var audio3=new Audio('sounds/snare.mp3');
+            var audio3=new Audio('snare.mp3');
             audio3.play();
         }
         else if (exp==="d"){
-            var audio4=new Audio('sounds/tom-1.mp3');
+            var audio4=new Audio('tom-1.mp3');
             audio4.play();
         }
         else if (exp==="j"){
-            var audio5=new Audio('sounds/tom-2.mp3');
+            var audio5=new Audio('tom-2.mp3');
             audio5.play();
         }
         else if (exp==="k"){
-            var audio6=new Audio('sounds/tom-3.mp3');
+            var audio6=new Audio('tom-3.mp3');
             audio6.play();
         }
         else{
-            var audio7=new Audio('sounds/tom-4.mp3');
+            var audio7=new Audio('tom-4.mp3');
             audio7.play();
         }
     });
@@ -36,31 +36,31 @@ for (var i = 0; i < n; i++) {
 document.addEventListener("keydown",function(event){
     var exp=event.key;
     if (exp==="w"){
-        var audio1=new Audio('sounds/crash.mp3');
+        var audio1=new Audio('crash.mp3');
         audio1.play();
     }
     else if (exp==="a"){
-        var audio2=new Audio('sounds/kick-bass.mp3');
+        var audio2=new Audio('kick-bass.mp3');
         audio2.play();
     }
     else if (exp==="s"){
-        var audio3=new Audio('sounds/snare.mp3');
+        var audio3=new Audio('snare.mp3');
         audio3.play();
     }
     else if (exp==="d"){
-        var audio4=new Audio('sounds/tom-1.mp3');
+        var audio4=new Audio('tom-1.mp3');
         audio4.play();
     }
     else if (exp==="j"){
-        var audio5=new Audio('sounds/tom-2.mp3');
+        var audio5=new Audio('tom-2.mp3');
         audio5.play();
     }
     else if (exp==="k"){
-        var audio6=new Audio('sounds/tom-3.mp3');
+        var audio6=new Audio('tom-3.mp3');
         audio6.play();
     }
     else{
-        var audio7=new Audio('sounds/tom-4.mp3');
+        var audio7=new Audio('tom-4.mp3');
         audio7.play();
     }
 })
